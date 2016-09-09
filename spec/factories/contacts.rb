c2c9dc9cfcd34a0e2480a108c4ee85a06831b775
Leonardo Graterol <pankas87@gmail.com>
2016-09-09 02:13:12 -0300

    ContactsController POST #create: Invalid attributes spec

--- spec/factories/contacts.rb
+++ spec/factories/contacts.rb
@@ -4,5 +4,10 @@ FactoryGirl.define do
     email { Faker::Internet.email }
     phone_number { Faker::PhoneNumber.phone_number }
     address { Faker::Address.street_address }
+
+    factory :invalid_contact do
+      full_name nil
+      address nil
+    end
   end
 end
